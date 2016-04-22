c94ca099436b30ce798d5995a0753fb6e316daf1
Luke Meyer <lmeyer@redhat.com>
2016-04-22 05:12:24 -0400

    deployer: more validations
    validates service accounts in preflight.
    WIP postdeployment validation for routes.

--- deployer/run.sh
+++ deployer/run.sh
@@ -9,6 +9,7 @@ mode=${MODE:-install}
 project=${PROJECT:-openshift-infra}
 scratch_dir=${SCRATCH_DIR:-_output}
 secret_dir=${SECRET_DIR:-_secret}
+ignore_preflight=${IGNORE_PREFLIGHT:-false}
 
 # following are useful in dev, but expected to fail inside container
 mkdir -p $scratch_dir && chmod 700 $scratch_dir && rm -rf $scratch_dir/* || :
@@ -21,12 +22,12 @@ case "${mode}" in
       validate_preflight
     ;;
   install|deploy)
-      validate_preflight
+      validate_preflight || [ "$ignore_preflight" = true ]
       run_installation
       validate_deployment
     ;;
   reinstall|redeploy)
-      validate_preflight
+      validate_preflight || [ "$ignore_preflight" = true ]
       delete_installation
       run_installation
       validate_deployment
@@ -35,11 +36,11 @@ case "${mode}" in
       delete_installation
     ;;
   validate)
-      validate_preflight
+      validate_preflight || [ "$ignore_preflight" = true ]
       validate_deployment
     ;;
   *)
-    echo "Invalid mode provided. One of [install|reinstall|uninstall|preflight|validate] was expected";
+    echo "Invalid mode provided. One of [preflight|install|reinstall|uninstall|validate] was expected";
     exit 1
     ;;
 esac

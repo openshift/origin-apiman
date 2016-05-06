3ecb8708a136405f77cf38b1d57b860fe4e0cc78
Luke Meyer <lmeyer@redhat.com>
2016-05-06 13:52:53 -0400

    apiman-builder: enable binary builds

--- apiman-builder/build.sh
+++ apiman-builder/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -euo pipefail
+env
+
+set -exuo pipefail
 
 if [ -z "${DOCKER_SOCKET:-}" ]; then
   echo "Docker socket not specified with DOCKER_SOCKET."
@@ -11,38 +13,43 @@ elif [ ! -e "${DOCKER_SOCKET}" ]; then
   exit 1
 fi
 
-tag=""
-[ -n "${OUTPUT_IMAGE:-}" ] && tag="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
+# get properly situated in the source code
+pushd $(mktemp --directory)
+if [ -z "${SOURCE_REPOSITORY:-}" ]; then
+  # assume source is binary tar input
+  tar zvfx - <&0
+else
+  # preflight check whether remote addr comes back in a reasonable time
+  if ! [[ $SOURCE_REPOSITORY =~ ^git[@:] ]] ; then
+    url="${SOURCE_REPOSITORY}"
+    [[ $url =~ ^https?:// ]] || url="https://${url}"
+    if ! curl --head --silent --fail --location --max-time 16 $url > /dev/null; then
+      echo "Could not access source url: ${SOURCE_REPOSITORY}"
+      exit 1
+    fi
+  fi
 
-# preflight check whether remote addr comes back in a reasonable time
-if ! [[ $SOURCE_REPOSITORY =~ ^git[@:] ]] ; then
-  url="${SOURCE_REPOSITORY}"
-  [[ $url =~ ^https?:// ]] || url="https://${url}"
-  if ! curl --head --silent --fail --location --max-time 16 $url > /dev/null; then
-    echo "Could not access source url: ${SOURCE_REPOSITORY}"
+  # actually check out the code
+  if ! git clone --recursive "${SOURCE_REPOSITORY}" . ; then
+    echo "Error trying to fetch git source: ${SOURCE_REPOSITORY}"
     exit 1
   fi
-fi
-
-# actually check out the code
-BUILD_DIR=$(mktemp --directory)
-if ! git clone --recursive "${SOURCE_REPOSITORY}" "${BUILD_DIR}"; then
-  echo "Error trying to fetch git source: ${SOURCE_REPOSITORY}"
-  exit 1
-fi
-pushd "${BUILD_DIR}"
-if [ -n "${SOURCE_REF:-}" ]; then
-  if ! git checkout "${SOURCE_REF}"; then
-    echo "Error trying to checkout branch: ${SOURCE_REF}"
-    exit 1
+  if [ -n "${SOURCE_REF:-}" ]; then
+    if ! git checkout "${SOURCE_REF}"; then
+      echo "Error trying to checkout branch: ${SOURCE_REF}"
+      exit 1
+    fi
   fi
 fi
-[ -n "${SOURCE_CONTEXT_DIR:-}" ] && cd $SOURCE_CONTEXT_DIR
+# SOURCE_CONTEXT_DIR is blank for binary build, extract from BUILD
+contextDir=$(echo -e "$BUILD" | jq -r .spec.source.contextDir)
+[ -n "$contextDir" ] && cd $contextDir
 
 # build and push
-if [ -n "${tag:-}" ]; then
-  [ -d /var/run/secrets/openshift.io/push ] && [ ! -e $HOME/.dockercfg ] && \
-    cp /var/run/secrets/openshift.io/push/.dockercfg $HOME/.dockercfg
+if [ -n "${OUTPUT_IMAGE:-}" ] ; then
+  tag="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
+  [ -d "${PUSH_DOCKERCFG_PATH:-}" ] && [ ! -e "$HOME/.dockercfg" ] && \
+    cp "$PUSH_DOCKERCFG_PATH/.dockercfg" "$HOME/.dockercfg"
   mvn -Pf8-build -Pssl -Ddocker.image="${tag}" && docker push "$tag"
 else
   mvn -Pf8-build -Pssl
